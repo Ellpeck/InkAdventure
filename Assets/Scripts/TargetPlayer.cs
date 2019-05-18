e1dbec3a0c4ab52b55e4b7499c8364d3a7135795
Ellpeck <megamaximal@gmail.com>
2019-05-18 11:38:13 +0200

    made fish flip when coming from the right

--- Assets/Scripts/TargetPlayer.cs
+++ Assets/Scripts/TargetPlayer.cs
@@ -1,13 +1,16 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class TargetPlayer : MonoBehaviour {
 
     public float speed;
 
+    private SpriteRenderer spriteRenderer;
     private Rigidbody2D body;
     private GameObject player;
 
     private void Start() {
+        this.spriteRenderer = this.GetComponentInChildren<SpriteRenderer>();
         this.body = this.GetComponent<Rigidbody2D>();
         this.player = GameObject.FindGameObjectWithTag("Player");
     }
@@ -16,8 +19,13 @@ public class TargetPlayer : MonoBehaviour {
         var trans = this.transform;
         var myPos = trans.position;
         var playerPos = this.player.transform.position;
-        trans.right = new Vector3(playerPos.x - myPos.x, playerPos.y - myPos.y);
 
+        var dist = new Vector3(playerPos.x - myPos.x, playerPos.y - myPos.y);
+        this.spriteRenderer.flipY = dist.x < 0;
+        trans.right = dist;
+    }
+
+    private void FixedUpdate() {
         this.body.AddRelativeForce(new Vector2(this.speed, 0));
     }
 
